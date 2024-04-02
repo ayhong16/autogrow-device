a864760d64e642daa9f992c614958154b2abe980
Ashley Hong <ayhong8@gmail.com>
2024-04-02 18:27:44 -0400

    Fix readme

--- main/src/wifiWrapper.h
+++ main/src/wifiWrapper.h
@@ -1,10 +1,10 @@
 #include <WiFi.h>
-#include "time.h"
-#include "secrets.h"
 
-class WiFiWrapper
-{
-public:
+#include "secrets.h"
+#include "time.h"
+
+class WiFiWrapper {
+   public:
     WiFiWrapper();
     ~WiFiWrapper(){};
 };
